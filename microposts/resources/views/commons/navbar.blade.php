6fcea3820eccee0e295e118d4261461fc26401f1
Ubuntu <ubuntu@ip-172-31-33-156.ap-northeast-1.compute.internal>
2020-10-17 06:52:57 +0000

    user pages

--- microposts/resources/views/commons/navbar.blade.php
+++ microposts/resources/views/commons/navbar.blade.php
@@ -12,16 +12,16 @@
             <ul class="navbar-nav">
                 @if (Auth::check())
                     {{-- ユーザ一覧ページへのリンク --}}
-                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item">{!! link_to_route('users.index', 'Users', [], ['class' => 'nav-link']) !!}</li>
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                         <ul class="dropdown-menu dropdown-menu-right">
-                            {{-- ユーザ詳細ページへのリンク --}}
-                            <li class="dropdown-item"><a href="#">My profile</a></li>
-                            <li class="dropdown-divider"></li>
-                            {{-- ログアウトへのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
-                        </ul>
+                                {{-- ユーザ詳細ページへのリンク --}}
+                                <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
+                                <li class="dropdown-divider"></li>
+                                {{-- ログアウトへのリンク --}}
+                                <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
                     </li>
                 @else
                     {{-- ユーザ登録ページへのリンク --}}
